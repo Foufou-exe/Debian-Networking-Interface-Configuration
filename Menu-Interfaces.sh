9f9ba8d4572d6887fc741e7a9b17e7efb656c20b
/Foufoudu34 <76502393+Foufou-exe@users.noreply.github.com>
2022-03-04 21:26:47 +0100

    Update Error code 🌍
    Update code

--- Menu-Interfaces.sh
+++ Menu-Interfaces.sh
@@ -5,10 +5,10 @@
 function main() {
    read -n2 -p "Choisi ta langue/Choose your language (FR/EN):" rep
    if [ $rep = "FR" ]; then
-      bash ~/Interface/FR/InterfacesNetworkFR.sh
+      bash ~/Networking-Interface-Configuration/Interface/FR/InterfacesNetworkFR.sh
       exit
    elif [ $rep = "EN" ]; then
-      bash ~/Interface/EN/InterfacesNetworkEN.sh
+      bash ~/Networking-Interface-Configuration/Interface/EN/InterfacesNetworkEN.sh
       exit
    fi
       echo -e "/nError: Choose your language,retry"
